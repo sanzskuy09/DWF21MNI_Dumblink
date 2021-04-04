5e43596678c96e5009c3d5b1e8abda5b991ccbfc
Ihsan <mn.ihsan09@gmail.com>
2021-04-04 14:25:19 +0700

    add LoginRegister

--- frontend/src/Component/HomePages/NavVertical.js
+++ frontend/src/Component/HomePages/NavVertical.js
@@ -1,10 +1,16 @@
 import { useContext } from "react";
+import { Redirect } from "react-router";
+import { Link, NavLink } from "react-router-dom";
 
 import { UserContext } from "../../Contexts/userContext";
 
 import brandName from "../../Assets/images/brand-name.svg";
-import { Redirect } from "react-router";
-import { Link, NavLink } from "react-router-dom";
+import templateImage from "../../Assets/images/template-img.svg";
+import profileImage from "../../Assets/images/profile-img.svg";
+import linkImage from "../../Assets/images/link-img.svg";
+import logoutImage from "../../Assets/images/logout-img.svg";
+
+import { Nav } from "react-bootstrap";
 
 const NavVertical = () => {
   const [state, dispatch] = useContext(UserContext);
@@ -26,23 +32,43 @@ const NavVertical = () => {
           backgroundColor: "#FFF",
         }}
       >
-        <nav class="nav d-flex flex-column justify-content-center">
-          <div class="mt-3" href="#">
-            <img src={brandName} alt="" />
+        <Nav className="nav d-flex flex-column align-items-baseline">
+          <div className="mt-3" href="#">
+            <img src={brandName} alt={brandName} />
           </div>
-          <Link class="mt-5 text-center" to="/template">
+          <Link
+            className="mt-5 text-center navbar-vertical-text d-flex"
+            style={{ textDecoration: "none", color: "#000" }}
+            to="/template"
+          >
+            <img src={templateImage} alt={templateImage} className="mr-3" />
             Template
           </Link>
-          <Link class="mt-4 text-center" to="/profile">
+          <Link
+            className="mt-4 text-center navbar-vertical-text d-flex"
+            style={{ textDecoration: "none", color: "#000" }}
+            to="/profile"
+          >
+            <img src={profileImage} alt={profileImage} className="mr-3" />
             Profil
           </Link>
-          <Link class="mt-4 text-center" to="/my-link">
+          <Link
+            className="mt-4 text-center navbar-vertical-text d-flex"
+            style={{ textDecoration: "none", color: "#000" }}
+            to="/my-link"
+          >
+            <img src={linkImage} alt={linkImage} className="mr-3" />
             My Link
           </Link>
-          <button class="btn mt-auto mb-5" onClick={Logout}>
+          <Link
+            className="mt-auto mb-5 navbar-vertical-text d-flex"
+            style={{ textDecoration: "none", color: "#000" }}
+            onClick={Logout}
+          >
+            <img src={logoutImage} alt={logoutImage} className="mr-3" />
             Logout
-          </button>
-        </nav>
+          </Link>
+        </Nav>
       </div>
     </>
   );
