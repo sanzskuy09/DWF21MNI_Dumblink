7da4f48c2771ffc9dec8409861b9cb903d87ec25
Ihsan <mn.ihsan09@gmail.com>
2021-04-16 17:00:56 +0700

    deploy to heroku

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const router = require("./src/routes");
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
